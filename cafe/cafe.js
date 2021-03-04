82f4a99536c8f763bf52d0803de997be305d192d
kat-goon <73501667+kat-goon@users.noreply.github.com>
2021-03-04 16:08:39 -0500

    Add files via upload
    -main changes to katspace.html and katspace.css- addition of another image+gif

--- cafe/cafe.js
+++ cafe/cafe.js
@@ -2,7 +2,7 @@
 var canvas = document.getElementById('myCanvas');
 var ctx;
 var w = 1440;
-var h = 900;
+var h = 2800;
 var colour = ["#F5F5DC", "#FFE4C4", "#FAEBD7"];
 var rn = rand(5);
 
@@ -15,7 +15,7 @@ setUpCanvas();
 
 
 
-for(var i=0; i<25; i++){
+for(var i=0; i<50; i++){
     arc(rand(w), rand(h), 10+rand(20), colour[i%colour.length]);
 }
 
@@ -42,5 +42,6 @@ function setUpCanvas(){
     canvas.width = w;
     canvas.height = h;
 }
+
 
 console.log("welcome to the cafe, please refresh the page to see the dancing lights")
